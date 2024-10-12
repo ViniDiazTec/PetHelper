45d2cea3774693d060078d546f46bf065bcbc370
Vini_Diaz <133820166+ViniDiazTec@users.noreply.github.com>
2024-10-12 11:44:09 -0300

    Inicio repo remoto

---
+++ src/test/java/com/helppet/PetHelper/PetHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helppet.PetHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
